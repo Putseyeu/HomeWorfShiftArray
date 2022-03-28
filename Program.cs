9253e4ed6ba11a7009f6d9c99340ea34a4a36a30
Putseyeu <putseyeu.a.d@gmail.com>
2022-03-28 15:27:10 +0200

    step

--- Program.cs
+++ Program.cs
@@ -10,34 +10,38 @@ namespace HomeWorfShiftArray
     {
         static void Main(string[] args)
         {
-            const int numberElement = 5;
-            int[] array = new int[numberElement] { 1, 2, 3, 4, 5 };
+            const int numberElement = 4;
+            int[] array = new int[numberElement] { 1, 2, 3, 4};
             int numberOffset;
+
             Console.WriteLine("Исходный массив:");
 
             for (int i = 0; i < array.Length; i++)
             {
                 Console.Write(" " + array[i]);
             }
+
             Console.WriteLine();
             Console.WriteLine("Введите число для сдвига");
             numberOffset = Convert.ToInt16(Console.ReadLine());
 
             for (int i = 0; i < numberOffset; i++)
             {
-                int numberLast = array[array.Length - 1];
-                for (int j = array.Length - 1; j > 0; j--)
+                int numberTemp = array[0];
+                for (int j = 0; j < array.Length - 1; j++)
                 {
-                    array[j] = array[j - 1];
+                    array[j] = array[j + 1];
                 }
-                array[0] = numberLast;
+                array[array.Length - 1] = numberTemp;
             }
 
             Console.WriteLine("Новый массив: ");
+
             for (int i = 0; i < array.Length; i++)
             {
                 Console.Write(" " + array[i]);
             }
+
             Console.WriteLine();
         }
     }
